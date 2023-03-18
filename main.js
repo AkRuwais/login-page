f55ed9b61e7923fd6a8f3c33f5cf9d593dbba43d
AkRuwais <akruwais9445@gmail.com>
2023-03-18 22:07:42 +0530

    photo upload

--- main.js
+++ main.js
@@ -4,22 +4,34 @@ app.listen(4000, () => {
   console.log("Server connected");
 });
 
+app.set("view engine", "ejs");
+
+const bodyparser = require("body-parser");
+const { urlencoded } = require("body-parser");
+app.use(urlencoded({ extended: true }));
+app.use(bodyparser.json());
+
 const mongoose = require("mongoose");
 mongoose.connect("mongodb://127.0.0.1:27017/login").then(() => {
   console.log("mongoose Connected");
 });
 
-const Import = require("./models/product");
+app.use(express.static(__dirname + "/public"));
+
+const L_router = require("./router/L_router");
+app.use("/", L_router);
+
+const router = require("./router/S_router");
+app.use("/Signup", router);
+
+const H_router = require("./router/H_router");
+app.use("/Home", H_router);
 
 const path = require("path");
 const { dirname } = require("path");
-app.set("viw engine", "ejs");
+
 app.use(
   express.urlencoded({
     extended: false,
   })
 );
-
-app.get("/", (req, res) => {
-  res.render(path.join(__dirname, "views/Signup.ejs"));
-});
